a7ba3bc19adf2e117c94496b2c90569a4bfc2b77
Amy Pu <amy_pu@brown.edu>
2019-04-02 00:21:00 -0400

    added in smaller images

--- static/javascript/interp.js
+++ static/javascript/interp.js
@@ -9,37 +9,80 @@ $(document).ready(() => {
     const $analysis2 = $("#analysis2");
     const $analysis3 = $("#analysis3");
     const $analysis4 = $("#analysis4");
+    const $undergrowth = $("#undergrowth");
+    const $grassy = $("#grassy");
+    const $morning = $("#morning");
+    const $black = $("#black");
+    const $woods = $("#woods");
+    const $yellow = $("#yellow-woods");
 
-    $part1.on("mouseover", function() {
-        $analysis1.show();
-        $(document.body).attr("style", "background-image: url('static/images/painting2.png')");
+    // $part1.on("mouseover", function() {
+    //     $analysis1.show();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting2.png')");
+    // });
+    // $part1.on("mouseout", function() {
+    //     $analysis1.hide();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    // });
+    // $part2.on("mouseover", function() {
+    //     $analysis2.show();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting3.png')");
+    // });
+    // $part2.on("mouseout", function() {
+    //     $analysis2.hide();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    // });
+    // $part3.on("mouseover", function() {
+    //     $analysis3.show();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting4.jpeg')");
+    // });
+    // $part3.on("mouseout", function() {
+    //     $analysis3.hide();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    // });
+    // $part4.on("mouseover", function() {
+    //     $analysis4.show();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting5.png')");
+    // });
+    // $part4.on("mouseout", function() {
+    //     $analysis4.hide();
+    //     $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    // });
+    $undergrowth.on("mouseover", function() {
+        console.log("aiyalskfjlskfjlskfj");
+        $(document.body).attr("style", "background-image: url('static/images/undergrowth.jpg')");
     });
-    $part1.on("mouseout", function() {
-        $analysis1.hide();
+    $undergrowth.on("mouseout", function() {
         $(document.body).attr("style", "background-image: url('static/images/painting.png')");
     });
-    $part2.on("mouseover", function() {
-        $analysis2.show();
-        $(document.body).attr("style", "background-image: url('static/images/painting3.png')");
+    $grassy.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/grassy.jpg')");
     });
-    $part2.on("mouseout", function() {
-        $analysis2.hide();
+    $grassy.on("mouseout", function() {
         $(document.body).attr("style", "background-image: url('static/images/painting.png')");
     });
-    $part3.on("mouseover", function() {
-        $analysis3.show();
-        $(document.body).attr("style", "background-image: url('static/images/painting4.jpeg')");
+    $morning.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/morning.jpg')");
     });
-    $part3.on("mouseout", function() {
-        $analysis3.hide();
+    $morning.on("mouseout", function() {
         $(document.body).attr("style", "background-image: url('static/images/painting.png')");
     });
-    $part4.on("mouseover", function() {
-        $analysis4.show();
-        $(document.body).attr("style", "background-image: url('static/images/painting5.png')");
+    $black.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/black.jpg')");
     });
-    $part4.on("mouseout", function() {
-        $analysis4.hide();
+    $black.on("mouseout", function() {
+        $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    });
+    $woods.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/woods.jpg')");
+    });
+    $woods.on("mouseout", function() {
+        $(document.body).attr("style", "background-image: url('static/images/painting.png')");
+    });
+    $yellow.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/yellow-woods.jpg')");
+    });
+    $yellow.on("mouseout", function() {
         $(document.body).attr("style", "background-image: url('static/images/painting.png')");
     });
 
@@ -150,4 +193,12 @@ $(document).ready(() => {
             $randomize.append($7);
             break;
     }
+
+    $frost = $("#frost");
+    $frost.on("mouseover", function() {
+        $(document.body).attr("style", "background-image: url('static/images/frost.jpeg')");
+    });
+    $frost.on("mouseout", function() {
+        $(document.body).attr("style", "background-image: url('static/images/painting.jpeg')");
+    });
 });
